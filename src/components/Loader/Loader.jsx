e207fcd50c9a7c032b28d0346b741b97f1031de0
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-04-25 09:19:48 +0300

    Fixed loader and added some styles

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,19 +1,19 @@
-import { MagnifyingGlass } from 'react-loader-spinner';
+import { ThreeDots } from 'react-loader-spinner';
 import { LoaderContainer } from './Loader.styled';
 
 export const Loader = () => {
   return (
     <LoaderContainer>
-      <MagnifyingGlass
-  visible={true}
-  height="80"
-  width="80"
-  ariaLabel="MagnifyingGlass-loading"
-  wrapperStyle={{}}
-  wrapperClass="MagnifyingGlass-wrapper"
-  glassColor = '#c0efff'
-  color = '#e15b64'
-/>
+      <ThreeDots 
+height="80" 
+width="80" 
+radius="9"
+color="red"
+ariaLabel="three-dots-loading"
+wrapperStyle={{}}
+wrapperClassName=""
+visible={true}
+ />
     </LoaderContainer>
   );
 };
